4669c3c89834c4a29cf797655c8d80092c07a6a8
Carmen Alvarez <c@rmen.ca>
2021-11-25 09:12:16 +0100

    Include the NON_NULL inclusion behavior globally

--- src/main/kotlin/ca/rmen/poetassistant/restservice/rhymer/model/WordRhymesModel.kt
+++ src/main/kotlin/ca/rmen/poetassistant/restservice/rhymer/model/WordRhymesModel.kt
@@ -19,12 +19,10 @@
 
 package ca.rmen.poetassistant.restservice.rhymer.model
 
-import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
-@JsonInclude(JsonInclude.Include.NON_NULL)
 data class WordRhymesModel(
     val variantNumber: Int,
     val stressRhymes: SyllableRhymesModel? = null,

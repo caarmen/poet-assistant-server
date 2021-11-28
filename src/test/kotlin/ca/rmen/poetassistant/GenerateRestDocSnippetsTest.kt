46f0975c90c3985b8afe43ee4fa42e4dace6608c
Carmen Alvarez <c@rmen.ca>
2021-11-28 17:20:25 +0100

    Generate documentation with pretty-print json snippets.
    Add a `Preprocessors.prettyPrint()` to tests which generate documentation snippets

--- src/test/kotlin/ca/rmen/poetassistant/GenerateRestDocSnippetsTest.kt
+++ src/test/kotlin/ca/rmen/poetassistant/GenerateRestDocSnippetsTest.kt
@@ -23,24 +23,21 @@ import ca.rmen.poetassistant.restservice.definitions.DefinitionController
 import ca.rmen.poetassistant.restservice.rhymes.RhymeController
 import ca.rmen.poetassistant.restservice.thesaurus.ThesaurusController
 import ca.rmen.poetassistant.restservice.wotd.WotdController
-import ca.rmen.poetassistant.GenerateRestDocSnippetsTest.Companion.REST_DOCS_OUTPUT_DIR
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
+import org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse
+import org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 
 @AutoConfigureMockMvc
 @SpringBootTest
-@AutoConfigureRestDocs(outputDir = REST_DOCS_OUTPUT_DIR)
+@AutoConfigureRestDocs(outputDir = "build/snippets")
 class GenerateRestDocSnippetsTest {
-    companion object {
-        const val REST_DOCS_OUTPUT_DIR = "build/snippets"
-        val String.serviceDocDir get() = removePrefix("/")
-    }
 
     @Autowired
     private lateinit var mockMvc: MockMvc
@@ -48,30 +45,35 @@ class GenerateRestDocSnippetsTest {
     @Test
     fun testGenerateRhymesSnippet() {
         mockMvc.perform(MockMvcRequestBuilders.get("${RhymeController.SERVICE}?${RhymeController.QUERY_PARAM_WORD}=dove"))
-            .andDo(document(RhymeController.SERVICE.serviceDocDir))
+            .andDo(documentPretty(RhymeController.SERVICE))
     }
 
     @Test
     fun testGenerateThesaurusSnippet() {
         mockMvc.perform(MockMvcRequestBuilders.get("${ThesaurusController.SERVICE}?${ThesaurusController.QUERY_PARAM_WORD}=happy"))
-            .andDo(document(ThesaurusController.SERVICE.serviceDocDir))
+            .andDo(documentPretty(ThesaurusController.SERVICE))
     }
 
     @Test
     fun testGenerateDefinitionsSnippet() {
         mockMvc.perform(MockMvcRequestBuilders.get("${DefinitionController.SERVICE}?${DefinitionController.QUERY_PARAM_WORD}=happy"))
-            .andDo(document(DefinitionController.SERVICE.serviceDocDir))
+            .andDo(documentPretty(DefinitionController.SERVICE))
     }
 
     @Test
     fun testGenerateWotdTodaySnippet() {
         mockMvc.perform(MockMvcRequestBuilders.get(WotdController.SERVICE))
-            .andDo(document("${WotdController.SERVICE.serviceDocDir}/today"))
+            .andDo(documentPretty("${WotdController.SERVICE}/today"))
     }
 
     @Test
     fun testGenerateWotdListSnippet() {
         mockMvc.perform(MockMvcRequestBuilders.get("${WotdController.SERVICE}?${WotdController.QUERY_PARAM_BEFORE}=2021-12-31&${WotdController.QUERY_PARAM_SIZE}=7"))
-            .andDo(document("${WotdController.SERVICE.serviceDocDir}/list"))
+            .andDo(documentPretty("${WotdController.SERVICE}/list"))
     }
+
+    private fun documentPretty(path: String) = document(
+        path.removePrefix("/"),
+        preprocessResponse(prettyPrint())
+    )
 }

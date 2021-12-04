00c31bed90abf7eb1ec0f914123a62d5166c83cb
Carmen Alvarez <c@rmen.ca>
2021-12-04 16:10:55 +0100

    Redirect the home page to the swagger doc

---
+++ modules/api/src/main/kotlin/ca/rmen/poetassistant/api/AppController.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2021 - present Carmen Alvarez
+ *
+ * This file is part of Poet Assistant.
+ *
+ * Poet Assistant is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Poet Assistant is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Poet Assistant.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ca.rmen.poetassistant.api
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.view.RedirectView
+
+@RestController
+class AppController() {
+
+    @GetMapping("/")
+    fun getRoot() = RedirectView("/swagger-ui.html")
+}
